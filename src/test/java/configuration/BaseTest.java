8ca8528a337c0305e0eb7bd4ce009a2ea1887211
Dariusz Szyca <daszyca@amazon.com>
2023-01-24 16:30:30 +0100

    options fix

--- src/test/java/configuration/BaseTest.java
+++ src/test/java/configuration/BaseTest.java
@@ -18,12 +18,14 @@ import java.time.Duration;
 public class BaseTest {
     public static Wait<WebDriver> wait;
     public static WebDriver driver;
-    public static String remote_url_firefox = "http://localhost:4444/wd/hub";
+    public static String remote_url_firefox = "http://localhost:4444";
     public static EnvConfig envConfig = ConfigFactory.create(EnvConfig.class);
 
     @BeforeAll
     public static void baseSetUp() throws MalformedURLException {
         FirefoxOptions options = new FirefoxOptions();
+        options.setBrowserVersion("109.0");
+        options.setPlatformName("LINUX");
         driver = new RemoteWebDriver(new URL(remote_url_firefox), options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
