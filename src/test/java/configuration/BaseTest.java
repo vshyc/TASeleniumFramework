9730050de45e4b7501d12830c96014fcd503fedf
Dariusz Szyca <daszyca@amazon.com>
2023-01-25 09:33:22 +0100

    docker tests

--- src/test/java/configuration/BaseTest.java
+++ src/test/java/configuration/BaseTest.java
@@ -11,7 +11,14 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import properties.EnvConfig;
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DockerClientBuilder;
+import org.testcontainers.DockerClientFactory;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL;
 
+
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
@@ -21,13 +28,15 @@ public class BaseTest {
     public static WebDriver driver;
     public static String remote_url_firefox = "http://localhost:4444";
     public static EnvConfig envConfig = ConfigFactory.create(EnvConfig.class);
+    private static BrowserWebDriverContainer firefox;
 
     @BeforeAll
     public static void baseSetUp() throws MalformedURLException {
-        FirefoxOptions options = new FirefoxOptions();
-        options.setBrowserVersion("109.0");
-        options.setPlatformName("LINUX");
-        driver = new RemoteWebDriver(new URL(remote_url_firefox), options);
+        firefox = new BrowserWebDriverContainer()
+                .withRecordingMode(RECORD_ALL, new File("target"))
+                .withCapabilities(new FirefoxOptions());
+        firefox.start();
+        driver = firefox.getWebDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         wait = new FluentWait<>(driver)
